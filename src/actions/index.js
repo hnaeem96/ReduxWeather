3b6bae6fa233de486c56623fed90df5006c87f65
Hassan Naeem <hassannaeem@Ralphs-iMac.lan>
2017-05-10 16:49:05 -0400

    Added axios library to make ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '5043076375508a87c12bf480aebc7a5a';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
