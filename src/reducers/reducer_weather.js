a76b7b0d836aea496b2485ba7429598a76a1cd4c
Hassan Naeem <hassannaeem@Ralphs-iMac.lan>
2017-05-11 09:46:42 -0400

    Concattenated arrays to help avoid state mutations in reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function(state =  null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state =  [], action) {
+  swith (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([action.payload.data]);
+      // ES5 version top, ES6 version at the bottom
+      return [ action.payload.data, ...state ];
+      // [city, city, city], NOT [city, [city, city]]
+  }
   return state;
 }
